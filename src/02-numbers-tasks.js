57dd314095f6a14bda6c11eecd71bc37c5189dc7
nikitabogdan10 <bogdannikita10@gmail.com>
2024-09-18 22:37:30 +0300

    27 completed

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -224,8 +224,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const parsedValue = parseFloat(value);
+  return Number.isNaN(parsedValue) ? def : parsedValue;
 }
 
 module.exports = {
