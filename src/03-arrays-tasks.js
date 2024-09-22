935500bd2a33d4d5e38f6fec9265cd565310cd9d
nikitabogdan10 <bogdannikita10@gmail.com>
2024-09-22 12:51:33 +0300

    52 completed

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -439,8 +439,9 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  // eslint-disable-next-line no-shadow, max-len
+  return Array.from({ length: n }, (_, i) => Array.from({ length: n }, (_, j) => (i === j ? 1 : 0)));
 }
 
 /**
