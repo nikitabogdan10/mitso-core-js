e2c85390c4551be8dd9dc16cb5c2cfcf915ae4f0
nikitabogdan10 <bogdannikita10@gmail.com>
2024-09-23 23:47:08 +0300

    67 completed

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -75,8 +75,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  const regex = new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{${minLength},}$`);
+  return regex;
 }
 
 module.exports = {
