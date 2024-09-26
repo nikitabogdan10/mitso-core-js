622291ea26909ec73ef983ad54392024610d0012
nikitabogdan10 <bogdannikita10@gmail.com>
2024-09-26 21:06:24 +0300

    87 completed

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -318,8 +318,16 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let str = num.toString();
+  while (str.length > 1) {
+    let sum = 0;
+    str.split('').forEach((char) => {
+      sum += parseInt(char, 10);
+    });
+    str = sum.toString();
+  }
+  return parseInt(str, 10);
 }
 
 /**
