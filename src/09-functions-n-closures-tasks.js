e7d80df7e01f91c16f246d926cec42ebbb2512bd
nikitabogdan10 <bogdannikita10@gmail.com>
2024-09-27 20:04:20 +0300

    100 completed

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -201,8 +201,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let nextId = startFrom;
+  return function generateId() {
+    const currentId = nextId;
+    nextId += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
